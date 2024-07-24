c5abf2009b200bf25a2d498933539633da6004f0
Maryna Karpova <mari.artarea@gmail.com>
2024-07-24 14:12:50 +0200

    HomePage

---
+++ src/components/HomePage/HomePage.tsx
@@ -0,0 +1,31 @@
+import s from './HomePage.module.scss';
+import WorldMap from '../../../public/img/World Map.svg';
+import Button from '../Button/Button';
+
+export const HomePage = () => {
+  return (
+    <div className={s.container}>
+      <div className={s.titleContainer}>
+        <h5 className={s.title}>
+          Empower with <br /> AQE Educate.
+        </h5>
+        <p className={s.paragraph}>
+          Your trusted partner in professional development <br /> for educators.
+        </p>
+      </div>
+      <img className={s.img} src={WorldMap} alt="worldMap" />
+      <div className={s.containerLinks}>
+        <a href="#" className={s.link}>
+          Malta
+        </a>
+        <a href="#" className={s.link}>
+          Canary
+        </a>
+        <a href="#" className={s.link}>
+          Ireland
+        </a>
+      </div>
+      <Button className={s.button}>Get info package</Button>
+    </div>
+  );
+};
