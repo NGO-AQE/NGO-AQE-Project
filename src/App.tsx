e26b45f9b703ca15896e86d4bcd1ab0e8c0681f8
Maryna Karpova <mari.artarea@gmail.com>
2024-07-29 09:20:45 +0200

    app

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
 import './App.scss';
+import { HomePage } from './components/HomePage/HomePage';
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <HomePage />
+    </>
+  );
 }
 
 export default App;
