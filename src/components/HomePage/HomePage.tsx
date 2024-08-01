0c78f10e84247cb64e34bb9f655baaf3fcb347e8
Maryna Karpova <mari.artarea@gmail.com>
2024-08-01 16:22:29 +0200

    test

--- src/components/HomePage/HomePage.tsx
+++ src/components/HomePage/HomePage.tsx
@@ -1,33 +1,40 @@
 import s from './HomePage.module.scss';
-import WorldMap from '../../../public/img/World Map.svg';
+import WorldMap from '/img/World Map.svg';
 import Button from '../Button/Button';
 
 export const HomePage = () => {
   return (
-    <div className={s.container}>
-      <div className={s.titleContainer}>
-        <h5 className={s.title}>
-          Empower with <br />
-          <span className={s.subtitle}>AQE Educate.</span>
-        </h5>
-
-        <p className={s.paragraph}>
+    // <section className="container">
+    <div className={s['home-page']}>
+      <div className={s['home-page__title-container']}>
+        <h1 className={s['home-page__title']}>
+          Empower with AQE Educate.
+          <div className={s['home-page__title-element']}></div>
+        </h1>
+        <p className={s['home-page__paragraph']}>
           Your trusted partner in professional development for educators.
         </p>
       </div>
-      <img className={s.img} src={WorldMap} alt="worldMap" />
-      <div className={s.containerLinks}>
-        <a href="#" className={s.link}>
+      <Button className={s['home-page__button-tablet']}>
+        Get info package
+      </Button>
+      <div className={s['home-page__img-wrapper']}>
+        {' '}
+        <img className={s['home-page__img']} src={WorldMap} alt="World Map" />
+      </div>
+      <div className={s['home-page__container-links']}>
+        <a href="#" className={s['home-page__link']}>
           Malta
         </a>
-        <a href="#" className={s.link}>
+        <a href="#" className={s['home-page__link']}>
           Canary
         </a>
-        <a href="#" className={s.link}>
+        <a href="#" className={s['home-page__link']}>
           Ireland
         </a>
       </div>
-      <Button className={s.button}>Get info package</Button>
+      <Button className={s['home-page__button']}>Get info package </Button>
     </div>
+    // </section>
   );
 };
