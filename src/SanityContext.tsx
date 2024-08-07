431888f782bb0b766fdd670920c4c3223526bbc5
Davwidos <d.jurkowski015@gmail.com>
2024-08-07 15:16:22 +0200

    refactor sanity context and parters section

--- src/SanityContext.tsx
+++ src/SanityContext.tsx
@@ -1,10 +1,11 @@
 import React, { createContext, useEffect, useState } from 'react';
 
-import { SanityData } from './SanityDataTypes';
-import { client } from './SanityClient';
+import { Language, PartnersSection } from './SanityDataTypes';
+import { fetchLanguages, fetchPartnersSection } from './utils/sanityFetch';
 
 export interface SanityContextType {
-  documents: SanityData | null;
+  partnersSection: PartnersSection | null;
+  languages: Language[] | null;
   loading: boolean;
   error: Error | null;
   selectedLanguage: string | null;
@@ -15,60 +16,69 @@ export const SanityContext = createContext<SanityContextType | undefined>(
   undefined,
 );
 
-const defaultLanguageId = '5c699810-5457-434d-a6d7-64c20e74ebb3';
+const defaultLanguageCode = 'en';
 
 const SanityProvider: React.FC<{ children: React.ReactNode }> = ({
   children,
 }) => {
-  const [selectedLanguage, setSelectedLanguage] = useState(defaultLanguageId); //this is just the language id
-  const [documents, setDocuments] = useState<SanityData | null>(null);
+  const [selectedLanguage, setSelectedLanguage] = useState(defaultLanguageCode);
+  const [languages, setLanguages] = useState<Language[] | null>([]);
+  const [partnersSection, setPartnersSection] =
+    useState<PartnersSection | null>(null);
   const [loading, setLoading] = useState<boolean>(true);
   const [error, setError] = useState<Error | null>(null);
 
   const changeLanguage = (newLangCode: string) => {
     if (!loading) {
-      const langs = documents?.language;
+      const langs = languages;
       const langCodes = langs?.map(l => l.code);
 
       if (langCodes?.includes(newLangCode) && langs) {
         window.history.replaceState({}, '', newLangCode);
-        setSelectedLanguage(langs[langCodes?.indexOf(newLangCode)]._id);
+        setSelectedLanguage(newLangCode);
       } else {
         window.history.replaceState({}, '', 'en');
-        setSelectedLanguage(defaultLanguageId);
+        setSelectedLanguage(defaultLanguageCode);
       }
     }
   };
 
   useEffect(() => {
-    (async () => {
-      try {
-        const newData = {} as SanityData;
-        const res = await client.fetch('*');
-
-        res.forEach((doc: { _type: string }) => {
-          if (!newData[doc._type]) {
-            newData[doc._type] = [];
-          }
-          newData[doc._type].push(doc);
-        });
-        setDocuments(newData);
-        setError(null);
-      } catch (err) {
+    fetchLanguages()
+      .then(data => setLanguages(data))
+      .catch(err => {
         console.error('Error fetching data:', err);
         setError(err as Error);
-      } finally {
-        setLoading(false);
-      }
-    })();
-  }, []); //add language if we should refetch data after it changes
+      })
+      .finally(() => setLoading(false));
+  }, []);
+
+  useEffect(() => {
+    if (!languages) {
+      return;
+    }
+
+    setLoading(true);
+
+    Promise.all([
+      fetchPartnersSection(selectedLanguage).then(data =>
+        setPartnersSection(data),
+      ),
+    ])
+      .catch(err => {
+        console.error('Error fetching data:', err);
+        setError(err as Error);
+      })
+      .finally(() => setLoading(false));
+  }, [languages, selectedLanguage]); //add language if we should refetch data after it changes
 
   return (
     <SanityContext.Provider
       value={{
         selectedLanguage,
         changeLanguage,
-        documents,
+        partnersSection,
+        languages,
         loading,
         error,
       }}
