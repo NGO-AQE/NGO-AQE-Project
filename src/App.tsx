7527a2957fdfcf1bfad82273564e1674bbd45b2b
Justin <thedarkvibe1@gmail.com.com>
2024-08-07 16:32:31 +0200

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,41 @@
 import './App.scss';
 
-import sendMail from './sendMail';
+import { BasicCard } from './components/Slider/SliderExample';
+import Header from './components/Header/Header';
+import { HomePage } from './components/HomePage/HomePage';
+import PartnersSection from './components/PartnersSection/PartnersSection';
+import { Slider } from './components/Slider';
+import WhyAQE from './components/WhyAQE/WhyAQE';
 import { useEffect } from 'react';
 import { useSanity } from './hooks/useSanity';
 
 function App() {
   const sanity = useSanity();
 
-  //quick showcase, will revert app on merge
-  sendMail(
-    prompt('Give email:') as string,
-    prompt('Give name:') as string,
-    prompt('Give country:') as string,
-  );
-
   useEffect(() => {
     sanity.changeLanguage(window.location.pathname.split('/').slice(-1)[0]);
   }, [sanity]);
 
-  return <></>;
+  return (
+    <>
+      <Header />
+      <HomePage />
+      <WhyAQE />
+      <Slider title="Succes stories" buttonsPlacment="title" slidesOtside>
+        {[...Array(10)].map((_, i) => (
+          <Slider.Slide key={i}>
+            <BasicCard width={262} index={i} withText />
+          </Slider.Slide>
+        ))}
+      </Slider>
+      <PartnersSection />
+    </>
+  );
+  return (
+    <>
+      <HomePage />
+    </>
+  );
 }
 
 export default App;
