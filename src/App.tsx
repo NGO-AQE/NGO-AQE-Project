877f49f1f7fdfd5d9cb9ff66e324a9e607873a19
Justin <thedarkvibe1@gmail.com.com>
2024-08-09 12:56:05 +0200

    done

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import './App.scss';
-import Header from './components/Header/Header';
+
 import { BasicCard } from './components/Slider/SliderExample';
+import { FaqSection } from './components/FaqSection/FaqSection';
+import Header from './components/Header/Header';
+import { HomePage } from './components/HomePage/HomePage';
 import PartnersSection from './components/PartnersSection/PartnersSection';
 import { Slider } from './components/Slider';
+import WhyAQE from './components/WhyAQE/WhyAQE';
 import { useEffect } from 'react';
 import { useSanity } from './hooks/useSanity';
-import { HomePage } from './components/HomePage/HomePage';
-import WhyAQE from './components/WhyAQE/WhyAQE';
 
 function App() {
   const sanity = useSanity();
@@ -28,6 +30,7 @@ function App() {
         ))}
       </Slider>
       <PartnersSection />
+      <FaqSection />
     </>
   );
 }
