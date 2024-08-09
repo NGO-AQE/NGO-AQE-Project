dbd8032c812a9652e4fa799d53f80e18ba5eb285
Maryna Karpova <mari.artarea@gmail.com>
2024-08-09 13:23:32 +0200

    desktop

--- src/components/ContactUs/ContactUs.tsx
+++ src/components/ContactUs/ContactUs.tsx
@@ -6,7 +6,7 @@ export const ContactUs = () => {
       <div className={s.contact}>
         <h1 className={s.contact__title}>Contact Us</h1>
         <address className={s.contact__address}>
-          <div className={s.contactAddressWrapper}>
+          <div>
             <h4 className={s.contact__label}>Email</h4>
             <a
               href="mailto:john.doe@example.com"
@@ -16,7 +16,13 @@ export const ContactUs = () => {
               john.doe@example.com
             </a>
           </div>
-          <div className={s.contactAddressWrapper}>
+          <div className={s.contact__officeDesktop}>
+            <h4 className={s.contact__label}>Office Hours</h4>
+            <p className={s.contact__description}>
+              Monday-Friday, 9:00 AM - 5:00 PM (excluding holidays)
+            </p>
+          </div>
+          <div>
             <h4 className={s.contact__label}>Address</h4>
             <a
               href="https://www.google.pl/maps/place/Main+St,+Queens,+NY,+Stany+Zjednoczone/@40.7359783,-73.8276689,17z/data=!3m1!4b1!4m6!3m5!1s0x89c260618039000d:0x4e4216fa03a650cf!8m2!3d40.7359743!4d-73.825094!16s%2Fm%2F0gvtrf7?hl=pl&entry=ttu"
@@ -26,13 +32,13 @@ export const ContactUs = () => {
               123 Main Street, Cityville, State, Zip Code
             </a>
           </div>
-          <div className={s.contactAddressWrapper}>
+          <div className={s.contact__officeMobile}>
             <h4 className={s.contact__label}>Office Hours</h4>
             <p className={s.contact__description}>
               Monday-Friday, 9:00 AM - 5:00 PM (excluding holidays)
             </p>
           </div>
-          <div className={s.contactAddressWrapper}>
+          <div>
             <h4 className={s.contact__label}>Phone Number</h4>
             <a
               href="tel:+15551234567"
