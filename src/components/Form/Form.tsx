9d0901853b8f5f58861c37b5ad569be91a4ee8c5
Paweł Kloc <pawelkloc89@gmail.com>
2024-08-19 08:16:09 +0200

    Watchers added

--- src/components/Form/Form.tsx
+++ src/components/Form/Form.tsx
@@ -14,9 +14,14 @@ type FormData = {
 const Form: React.FC = () => {
   const {
     register,
+    watch,
     handleSubmit,
     formState: { errors },
   } = useForm<FormData>({ criteriaMode: 'all' });
+
+  const watchFullName = watch('fullName');
+  const watchEmail = watch('email');
+  const watchCountry = watch('country');
 
   const onSubmit = (data: FormData) => {
     sendMail(data.email, data.fullName, data.country).then(data =>
@@ -25,79 +30,86 @@ const Form: React.FC = () => {
   };
 
   return (
-    <section id="form" className={`section ${styles.section}`}>
-      <h6 className={`section__title ${styles.section__title}`}>
-        Get more in our info package
-      </h6>
-      <p className={`section__description ${styles.section__description}`}>
-        Fill the form and we`ll send you a file with actual information
-      </p>
-
-      <form
-        className={`container ${styles.container}`}
-        onSubmit={handleSubmit(onSubmit)}
-      >
-        <div className={styles.section__field}>
-          <label className={styles.section__subtitle} htmlFor="fullName*">
-            Full Name*
-          </label>
-          <input
-            className={styles.section__input}
-            {...register('fullName', { required: true })}
-            placeholder="John Doe"
-          />
-          {errors.fullName && (
-            <span className={styles.section__errorMessage}>Error name</span>
-          )}
+      <section id="form" className={`section ${styles.section}`}>
+        <div className={styles.section__titleContainer}>
+          <h6 className={`section__title ${styles.section__title}`}>
+            Get more in our info package
+          </h6>
+        </div>
+        <div className={styles.section__descriptionContainer}>
+          <p className={`section__description ${styles.section__description}`}>
+            Fill the form and we`ll send you a file with actual information
+          </p>
         </div>
 
-        <div className={styles.section__field}>
-          <label className={styles.section__subtitle} htmlFor="email*">
-            Email*
-          </label>
-          <input
-            className={styles.section__input}
-            {...register('email', {
-              required: 'This field is required',
-              pattern: {
-                value: /^[\w-]+(\.[\w-]+)*@([\w-]+\.)+[a-zA-Z]{2,7}$/,
-                message: 'Entered value does not match email format',
-              },
-            })}
-            placeholder="aqe@email.com"
-          />
-          {errors.email && (
-            <span className={styles.section__errorMessage}>
+        <form
+          className={`container ${styles.container}`}
+          onSubmit={handleSubmit(onSubmit)}
+        >
+          <div className={styles.section__field}>
+            <label className={styles.section__subtitle} htmlFor="fullName*">
+              Full Name*
+            </label>
+            <input
+              className={styles.section__input}
+              {...register('fullName', { required: true })}
+              placeholder="John Doe"
+              style={{ borderColor: watchFullName ? 'color.$primary-color' : 'color.$gray-light' }}
+            />
+            {errors.fullName && (
+              <span className={styles.section__errorMessage}>Error name</span>
+            )}
+          </div>
+
+          <div className={styles.section__field}>
+            <label className={styles.section__subtitle} htmlFor="email*">
+              Email*
+            </label>
+            <input
+              className={styles.section__input}
+              {...register('email', {
+                required: 'This field is required',
+                pattern: {
+                  value: /^[\w-]+(\.[\w-]+)*@([\w-]+\.)+[a-zA-Z]{2,7}$/,
+                  message: 'Entered value does not match email format',
+                },
+              })}
+              placeholder="aqe@email.com"
+              style={{ borderColor: watchEmail ? 'color.$primary-color' : 'color.$gray-light' }}
+            />
+            {errors.email && (
+              <span className={styles.section__errorMessage}>
               {errors.email.message}
             </span>
-          )}
-        </div>
+            )}
+          </div>
 
-        <div className={styles.section__field}>
-          <label className={styles.section__subtitle} htmlFor="country">
-            Country
-          </label>
-          <input
-            className={styles.section__input}
-            {...register('country')}
-            placeholder="Poland"
-          />
-        </div>
+          <div className={styles.section__field}>
+            <label className={styles.section__subtitle} htmlFor="country">
+              Country
+            </label>
+            <input
+              className={styles.section__input}
+              {...register('country')}
+              placeholder="Poland"
+              style={{ borderColor: watchCountry ? 'color.$primary-color' : 'color.$gray-light' }}
+            />
+          </div>
 
-        <div className={styles.section__agreement}>
-          <label className={styles.section__subtitle} htmlFor="agree">
-            <input {...register('agree')} type="checkbox" />
-            <span className={styles.section__agree}>
+          <div className={styles.section__agreement}>
+            <label className={styles.section__subtitle} htmlFor="agree">
+              <input {...register('agree')} type="checkbox" />
+              <span className={styles.section__agree}>
               I agree to receive information about the further courses from AQE.
             </span>
-          </label>
-        </div>
+            </label>
+          </div>
 
-        <Button className={styles.section__button} type="submit">
-          Get info package
-        </Button>
-      </form>
-    </section>
+          <Button className={styles.section__button} type="submit">
+            Get info package
+          </Button>
+        </form>
+      </section>
   );
 };
 
