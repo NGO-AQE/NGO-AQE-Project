1f7d415fc15725dd00ffc2c447ebb7c043773a0f
Maryna Karpova <mari.artarea@gmail.com>
2024-08-21 10:59:22 +0200

    section

--- src/components/Slider/Slider.tsx
+++ src/components/Slider/Slider.tsx
@@ -58,7 +58,7 @@ export const Slider: FC<Props> & SubComponents = ({
 
   return (
     <SliderContext.Provider value={contextValue}>
-      <div id={id} className={`flex flex-col`}>
+      <div id={id} className={`flex flex-col ${styles.section}`}>
         <div className={styles.container}>
           <div className={styles.slider__header}>
             <h2 className={styles.section__title}>{title}</h2>
