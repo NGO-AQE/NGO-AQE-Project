5b1e452fdfe4051d6c32223c22376e073a72aa24
Maryna Karpova <mari.artarea@gmail.com>
2024-08-26 11:25:16 +0200

    center partner cestion

--- src/components/PartnersSection/PartnersSection.tsx
+++ src/components/PartnersSection/PartnersSection.tsx
@@ -6,27 +6,27 @@ const PartnersSection = () => {
   const { partnersSection } = useSanity();
 
   if (!partnersSection) {
-    return;
+    return <div>No avaliable partners.</div>;
   }
 
   return (
-    <Slider
-      id="partners"
-      title={partnersSection.title}
-      buttonsPlacment="title"
-      slidesOtside
-    >
-      {partnersSection.partners.map(partner => {
-        return (
+    <div className={styles.sliderContainer}>
+      <Slider
+        id="partners"
+        title={partnersSection.title}
+        buttonsPlacment="title"
+        slidesOtside
+      >
+        {partnersSection.partners.map(partner => (
           <Slider.Slide key={partner._id}>
             <div className={styles.slide}>
               <img src={partner.image} alt={`${partner.name} image`} />
               <p className={styles.description}>{partner.name}</p>
             </div>
           </Slider.Slide>
-        );
-      })}
-    </Slider>
+        ))}
+      </Slider>
+    </div>
   );
 };
 
